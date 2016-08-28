fef4369663787ac61e2216d0cc3f24fd1d75ed35
Rebecca Allen <rebeccazarsky@gmail.com>
2016-08-28 15:14:45 -0400

    add more variables and objects to further functionality of scripts.js, clean up naming and slight adjustments to index.html to match scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
 //Business Logic
 
-var User = function(firstName, lastName, streetAddress, cityAddress, stateAddress) {
+var Name = function(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
+}
+
+var Address = function(streetAddress, cityAddress, stateAddress){
   this.streetAddress = streetAddress;
   this.cityAddress = cityAddress;
   this.stateAddress = stateAddress;
 }
 
-var Pizza = function(pizzaSize, pizzaTopping) {
-  this.pizzaSize = pizzaSize
-  this.pizzaTopping = pizzaTopping
-}
-
- Name.prototype.fullName = function() {
-   return this.firstName + " " + this.lastName
+ Name.prototype.fullName = function(){
+   return "<p>" + this.firstName + " " + this.lastName + "</p>"
  }
 
-Address.prototype.fullAddress = function() {
-  return "<ul><li>" + this.streetAddress + "</li><li>" + this.cityAddress + "</li><li>" + this.stateAddress + "</li></ul>"
+Address.prototype.fullAddress = function(){
+  return "<ul>" + "<li>" + this.streetAddress + "</li>" + "<li>" + this.cityAddress + "</li>" + "<li>" + this.stateAddress + "</li>" + "</ul>"
 }
 
+var Pizza = function(pizzaSize, Toppings){
+  this.pizzaSize = pizzaSize
+  this.Toppings = []
+}
 
+var Toppings = function(){}
 
-
+Pizza.prototype.fullPizza = function(){
+  return this.Toppings
+}
 
 
 //User Logic
@@ -32,15 +37,24 @@ $(document).ready(function(){
   $("form#delivery-info").submit(function(event){
     event.preventDefault();
 
+    var inputtedFirstName = $("input.first-name").val();
+    var inputtedLastName = $("input.last-name").val();
+    var newName = new Name(inputtedFirstName, inputtedLastName)
 
+    var inputtedStreet = $("input.street").val();
+    var inputtedCity = $("input.city").val();
+    var inputtedState = $("input.state").val();
+    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
 
-});
-
-  $("form#pizzas").submit(function(event){
-    event.preventDefault();
-
-  $("")
-
+    $(".full-name").text(newName.fullName());
+    $(".full-address").text(newAddress.fullAddress());
 
   });
+
+  // $("form#pizzas").submit(function(event){
+  //   event.preventDefault();
+  //   $("")
+  //
+  //
+  // });
 });
